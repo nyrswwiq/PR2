d92302158e5e2e299a4fd0cfe313283cb597af22
nyrswwiq <nastyakhila@gmail.com>
2026-04-20 14:06:29 +0600

    123

---
+++ 922-master/Практическая работа № 2/Задание_1/sample/index.php
@@ -0,0 +1,16 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="ru">';
+echo '<head>';
+echo '<meta charset="UTF-8">';
+echo '<title>Программирование на языке PHP</title>';
+echo '</head>';
+echo '<body>';
+echo '<h1>Основы программирования</h1>';
+echo '<h2>Введение в PHP</h2>';
+echo '<hr>';
+echo '<h2>Начинаем программировать в среде PHP</h2>';
+echo '</body>';
+echo '</html>';
+?>
+
---
+++ 922-master/Практическая работа № 2/Задание_4/sample/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	
+	<h1>Основы программирования</h1>
+	<h2>Переменные</h2>
+	
+	<?php
+		$var1 = 952;
+		$var2 = 4;
+		$var3 = 3;
+		$var4 = 476;
+		$var5 = 7;
+		$var6 = 196;
+
+		$result = ($var1/$var2)*$var3-($var4/$var5)+$var6;
+
+		echo $result;
+
+	?>
+	
+
+</body>
+</html>
